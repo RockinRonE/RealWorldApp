f629191a1f7b9c500eea0209ad8cb69a83e13470
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-06-22 16:56:28 -0500

    removed App.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux'; 
-import App from './components/App';
 import store from './store';
+
 import Home from './components/Home';
 import Login from './components/Login';
 import Header from './components/Header';
 
-// import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-
-
-// ReactDOM.render((
-// 	<Provider store={store}>
-// 		<Router history={hashHistory}>
-// 			<Route path="/" component={App}>
-// 				<IndexRoute component={Home} />
-// 				<Route path="login" component={Login} />
-// 			</Route>
-// 		</Router>
-// 	</Provider> 
-// ), document.getElementById('root'));
 
 ReactDOM.render((
 	<Provider store={store}>
